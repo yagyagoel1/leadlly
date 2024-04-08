ae4677b518bd397b87e403bd454f70682eff94dd
yagya <yagyagoel87@gmail.com>
2024-04-08 12:03:05 +0530

    added middlewares and also added the connection to db

---
+++ src/db/index.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+import { DB_Name } from "../constraints"
+
+ const connectDB =async ()=>{
+   try {
+    const connectionString = await mongoose.connect(`${process.env.DB_URL}/${DB_Name}`)
+    console.log(`DataBase is connected ${connectionString.connection.host}`)
+
+   } catch (error) {
+    console.log("There was some error while connecting to mongodb database",error)
+    process.exit(1)   
+} 
+}
+export default connectDB
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv"
+import connectDB from "./db"
+import { app } from "./app"
+dotenv.config({
+    path : "../env"
+})
+
+connectDB().then(()=>
+    {try {
+        app.on("error",()=>{
+            throw Error("there was some error using app");
+        })
+        app.listen(process.env.PORT||8000)
+        console.log(`server is running at port: ${process.env.PORT}`)
+    } catch (error) {
+        
+    }
+}
+)
