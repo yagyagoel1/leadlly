9895d55be55ee24328b5008afa4e8db679d1c4dd
yagya <yagyagoel87@gmail.com>
2024-04-10 00:30:42 +0530

    added the model for product

---
+++ src/models/Product.model.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const productSchema= new Schema({
+    name : {
+        type : String,
+        required: true,
+        trim : true
+    },
+    description: {
+        type:  String,
+        required : true,
+        trim :true
+    },
+    photo:  {
+        type: String,
+        required :true,
+        trim : true
+    },
+    owner: {
+        type :mongoose.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
+
+export const Product = mongoose.model("Product",productSchema)
